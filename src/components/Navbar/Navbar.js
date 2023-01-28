fcaaa2e4a0e6ba28d1477732f5895c5978fb409c
SmartDeveloper741 <matejag048@gmail.com>
2023-01-28 12:42:49 -0500

    feat: change the name

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -145,7 +145,7 @@ export const Navbar = () => {
         <a
           data-navbar-item
           className={styles.logo}
-          aria-label="Alexandre Andre, Designer"
+          aria-label="Mateja Gegic, Designer"
           onClick={handleMobileNavClick}
         >
           <Monogram highlight />
